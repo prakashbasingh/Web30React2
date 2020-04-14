e99d84a2207fab957f1126c3b7d8bbc5b337297b
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-14 19:03:42 +0200

    wip

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 export default function Search({ setSearchTerm }) {
   // On 'change' of the input we should set a new search term in app state
+  const changeHandler = event => {
+    const searchTerm = event.target.value
+    setSearchTerm(searchTerm)
+  }
+
   return (
     <div className='search-friends container'>
-      Search for a friend: <input />
+      Search for a friend: <input onChange={changeHandler} />
     </div>
   )
 }
