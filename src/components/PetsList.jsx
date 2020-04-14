d5e74f1c2df535e4e0ecc1da752cb67bcb85b814
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-14 19:54:18 +0200

    wip

--- src/components/PetsList.jsx
+++ src/components/PetsList.jsx
@@ -7,6 +7,9 @@ export default function PetsList({ pets }) {
       <div>The List of pets</div>
       {/* Loop over the pets generating a Pet element as you go */}
       {/* What props does the Pet component expect? */}
+      {
+        pets.map(pet => <Pet pet={pet} key={pet.id} />)
+      }
     </div>
   )
 }
