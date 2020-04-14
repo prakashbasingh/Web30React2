0a5cab8ad199dbfe1d9657d49e92b1dbb4a92661
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-14 18:26:58 +0200

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FriendsList from './FriendsList'
 import Search from './Search'
+import friendsData from '../dummy-data/friends'
+
 
 export default function App() {
-  // we need to keep track of the current search term
   // we need to keep track of the list of friends
+  // we need to keep track of the current search term
   return (
     <div className='app-friends container'>
       <h1>Friends App</h1>
--- src/dummy-data/friends.js
+++ src/dummy-data/friends.js
@@ -1,5 +1,7 @@
 import { v4 as uuid } from 'uuid'
 
+export const hello = 'world'
+
 export default [
   {
     id: uuid(),
